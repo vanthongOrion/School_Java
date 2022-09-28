207daee660f89dc6dc5055d26018ea6a2a669a18
IT21029 <vanthongvision977@gmail.com>
2022-09-28 11:47:15 +0900

    new

--- 9_28/Code9_28_3/Main.java
+++ 9_28/Code9_28_3/Main.java
@@ -1,16 +1,24 @@
-// import java.io.*;
+import java.io.*;
 public class Main {
 
    public static void main(String args[]) {
-      String Str = new String("Welcome to Tutorialspoint.com");
+      // String Str = new String("Welcome to Tutorialspoint.com");
 
-      System.out.print("Return Value :" );
-      System.out.println(Str.matches("(.*)Tutorials(.*)"));
+      // System.out.print("Return Value :" );
+      // System.out.println(Str.matches("(.*)Tutorials(.*)"));
 
-      System.out.print("Return Value :" );
-      System.out.println(Str.matches("Tutorials"));
+      // System.out.print("Return Value :" );
+      // System.out.println(Str.matches("Tutorials"));
 
-      System.out.print("Return Value :" );
-      System.out.println(Str.matches("Welcome(.*)"));
+      // System.out.print("Return Value :" );
+      // System.out.println(Str.matches("Welcome(.*)"));
+
+      public boolean isValidPlayerName(String name) {
+         return name.matches("[A-Z][A-Z0-9]{7}");
+      }
+
+      String str = new String("Vanthong")
+;
+      System.out.println(isValidPlayerName(str));
    }
 }
