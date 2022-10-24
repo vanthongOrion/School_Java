72f6a1f97866b600e09f70339f15db0d76576632
IT21029 <vanthongvision977@gmail.com>
2022-10-24 09:29:20 +0900

    new commit

--- 10_5/Hero.java
+++ 10_5/Hero.java
@@ -1,3 +1,14 @@
 public class Hero {
 	public String name;
+	public boolean equals(Object o) {
+		if (o == this) return true;
+		if (o == null) return false;
+		if (!(o instanceof Hero)) return false;
+		Hero h = (Hero) o;
+
+		if (!this.name.trim().equals(h.name.trim())){
+			return false;
+		}
+		return true;
+	}
 }
