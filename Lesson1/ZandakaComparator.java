6ab435d60cedc17740c21693216ee231139be02f
IT21029 <vanthongvision977@gmail.com>
2022-11-02 11:36:56 +0900

    11/02 newcommit

---
+++ Lesson1/ZandakaComparator.java
@@ -0,0 +1,11 @@
+package bank;
+
+
+import java.util.Comparator;
+
+
+public class ZandakaComparator implements Comparator<Account> {
+	public int compare(Account x, Account y) {
+		return (x.zandaka - y.zandaka);
+	}
+}
