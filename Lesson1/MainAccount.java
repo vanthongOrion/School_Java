3698f7d67f76353c71f7be096626c013b316960e
IT21029 <vanthongvision977@gmail.com>
2022-11-02 11:38:14 +0900

    11/02 commit

--- Lesson1/MainAccount.java
+++ Lesson1/MainAccount.java
@@ -8,18 +8,33 @@ public class MainAccount {
 		Account ac1 = new Account();
 		ac1.accountNo = "ac1";
 		ac1.number = 300;
-		ac1.zandaka = 200;
+		ac1.zandaka = 400;
 		Account ac2 = new Account();
 		ac2.accountNo = "ac2";
 		ac2.number = 200;
 		ac2.zandaka = 300;
 
-		List<Account> list = new ArrayList<>();
-		list.add(ac1);
-		list.add(ac2);
+		//using collectins.sort to sort list
 
-		Collections.sort(list, new ZandakaComparator());
-		System.out.println(list);
+		// List<Account> list = new ArrayList<>();
+		// list.add(ac1);
+		// list.add(ac2);
+
+		// Collections.sort(list, new ZandakaComparator());
+		// System.out.println(list);
+
+
+		//using treeset to sort list through ZandakaComparator
+		// TreeSet<Account> accounts1 = new TreeSet<>();
+		// accounts1.add(ac1);
+		// accounts1.add(ac2);
+
+		// System.out.println(accounts1);
+
+		TreeSet<Account> accounts2 = new TreeSet<>(new ZandakaComparator());
+		accounts2.add(ac1);
+		accounts2.add(ac2);
+		System.out.println(accounts2);
 
 
 
